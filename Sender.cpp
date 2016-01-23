39147cf5b6d00dae75f7c95b658cfa0adbbb5382
patvice <patrickgvice@gmail.com>
2016-01-23 16:59:54 -0500

    had the sender class allocte/deallocte the buff memory

--- Sender.cpp
+++ Sender.cpp
@@ -8,28 +8,43 @@
 
 using namespace std;
 
+
+Sender::Sender(int headerLen, int messageLen){
+   char b[headerLen+messageLen];
+   buff = &b;
+}
+
+Sender::~Sender(){
+   delete buff;
+}
+
+Sender::buf(){
+   return buff;
+}
+
+
 int main(int argc, char ** argv) {
+   // char a[]="ff:ff:ff:ff:ff:ff"; // broadcast address
+   char address[]="1c:bd:b9:7e:b6:5a"; // unicast address
+   char message[]="This is a test frame"; // data
+
+
+   int headerLen = aWLAN->getAddressLength()
    // create a sender
-   Sender* aSender=new Sender();
+   Sender* aSender=new Sender(headerLen, strlen(message));
    // create a wireless network abstraction
    WLAN* aWLAN=new WLAN(LABEL);
    // initialize
    aWLAN->init();
-   // send a frame
-   // char a[]="ff:ff:ff:ff:ff:ff"; // broadcast address
-   char address[]="1c:bd:b9:7e:b6:5a"; // unicast address
-   char message[]="This is a test frame"; // data
 
    // Build Address
    WLAN::WLANAddr daddr;
    aWLAN->buildHeader(address, &daddr);
 
-   char buff[aWLAN->getAddressLength()+strlen(message)];
-
    // memmove probably need to be changed somehow
-   memmove(buff, &hdr, WLAN_HEADER_LEN);
-   memmove(buff+WLAN_HEADER_LEN, message, strlen(message));
+   memmove(aSender->buff(), &hdr, WLAN_HEADER_LEN);
+   memmove(aSender->buff()+headerLen, message, strlen(message));
 
-   aWLAN->send(&buff, &daddr);
+   aWLAN->send(aSender->buff(), &daddr);
    return 0;
 }
